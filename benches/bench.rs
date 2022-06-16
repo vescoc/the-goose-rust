1c328eccaf43c923d901b4f43e3ad3d778e9777a
Vesco Claudio <claudio.vesco@gmail.com>
2022-06-16 23:38:59 +0200

    benches

---
+++ benches/bench.rs
@@ -0,0 +1,38 @@
+use criterion::{criterion_group, criterion_main, Criterion};
+
+use the_goose_rust::sample::*;
+use the_goose_rust::*;
+
+fn criterion_benchmark(c: &mut Criterion) {
+    c.bench_function("play game", |b| {
+        b.iter(|| {
+            let mut the_goose = SimpleTheGoose::<u32, _>::new((1u32..7u32).cycle());
+
+            let players = vec!["Pippo", "Pluto", "Paperino"];
+            for player in &players {
+                the_goose
+                    .execute(Command::Add(player))
+                    .expect("Adding player");
+            }
+
+            'outher: loop {
+                for player in &players {
+                    if the_goose
+                        .execute(Command::RollAndMove(player))
+                        .unwrap()
+                        .iter()
+                        .any(|event| match event {
+                            Event::Win(_) => true,
+                            _ => false,
+                        })
+                    {
+                        break 'outher;
+                    }
+                }
+            }
+        })
+    });
+}
+
+criterion_group!(benches, criterion_benchmark);
+criterion_main!(benches);
